180e5cf0513dbcc75f24c599c6d7aed31d5dfcc5
InfinitY <InfinitY@DESKTOP-BNQJILC>
2022-07-11 21:16:51 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApplication32/ConsoleApplication32.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <cassert>
+enum suit{ SPADES, CLUBS, DIAMONDS, HEARTS };
+enum ECardValue {
+    ACE = 1,
+    TWO = 2,
+    THREE = 3,
+    FOUR = 4,
+    FIVE = 5,
+    SIX = 6,
+    SEVEN = 7,
+    EIGHT = 8,
+    NINE = 9,
+    TEN = 10,
+    JACK = 10,
+    QUEEN = 10,
+    KING = 10
+};
+class Card {
+    ECardSuit suit;
+    ECardValue value;
+    bool isCoverUp;
+public:
+    Card(ECardSuit _suit, ECardValue _value) : suit(_suit), value(_value) {
+        isCoverUp = true;
+    }
+
+    void flip() {
+        isCoverUp = !isCoverUp;
+    }
+    ECardValue getValue() const {
+        return value;
+    }
+};
+
+int main() { // 1TBS
+   
+
+    Card c0(SPADES, ACE);
+    std::cout << c0.getValue() << std::endl;
+}
